2d9f34dbd6ab876c394e7262487d04841c1ba332
Sean Shookman <sms112788@gmail.com>
2016-08-09 21:44:15 -0500

    Fixing script to run from outside location

--- set-wallpaper.py
+++ set-wallpaper.py
@@ -5,5 +5,5 @@ import os
 dirPath = os.path.dirname(os.path.realpath(__file__))
 apodHTML = urllib.urlopen("http://apod.nasa.gov/apod/astropix.html").read()
 imageURI = re.search('(?<=href=")image/\d+/[a-zA-Z0-9_.]+', apodHTML)
-apodImage = urllib.urlretrieve('http://apod.nasa.gov/apod/'+imageURI.group(0), "wallpaper.jpg")
+apodImage = urllib.urlretrieve('http://apod.nasa.gov/apod/'+imageURI.group(0), dirPath+"/wallpaper.jpg")
 os.system("gsettings set org.gnome.desktop.background picture-uri file://"+dirPath+"/wallpaper.jpg")
